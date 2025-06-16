2660cd6c5bbb73482abd574c8ffc2edadd04ac25
S҉H҉I҉N҉C҉H҉O҉Y҉O҉N҉ ҉B҉A҉R҉U҉A҉ ҉A҉D҉I҉R҉T҉T҉A҉ <madman4205666@gmail.com>
2025-06-16 20:18:32 +0600

    Create FIRE-EYE.sh

---
+++ FIRE-EYE.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# FIRE-EYE Installer by ADIRTTA
+
+clear
+echo "🔥 Installing FIRE-EYE Tool..."
+sleep 1
+
+# Install required packages
+pkg update -y && pkg install git python wget -y
+pip install requests rich pyfiglet colorama
+
+# Clone the repo
+git clone https://github.com/ADIRTTA/FIRE-EYE.git $HOME/.fireeye
+
+# Create launcher command
+echo -e '#!/bin/bash\npython3 $HOME/.fireeye/FIRE-EYE.py' > $PREFIX/bin/FIRE-EYE
+chmod +x $PREFIX/bin/FIRE-EYE
+
+# Success message
+echo -e "\n✅ Installed successfully!"
+echo "🔓 Type 'FIRE-EYE' to start scanning!"
